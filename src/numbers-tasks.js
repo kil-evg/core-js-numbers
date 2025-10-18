89e81094ff1ef1a31e77ee7ef9a4e6849a6df048
EKil <loreleyko@gmail.com>
2025-10-19 00:15:57 +0200

    commit

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -196,8 +196,11 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
@@ -215,8 +218,9 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const num = Number(value);
+  return Number.isFinite(num) ? num : def;
 }
 
 /**
